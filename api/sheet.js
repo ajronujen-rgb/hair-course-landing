1e43fc1509e3ff883c67ba6456b09fd11bfd73a9
Anna <ajronujen@gmail.com>
2026-06-16 10:19:16 +0500

    fix: vercel.json, module.exports for api function

--- api/sheet.js
+++ api/sheet.js
@@ -3,7 +3,7 @@ const { JWT } = require('google-auth-library');
 
 const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
